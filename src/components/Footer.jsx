5ec200837200a41734066c414c2585799ad339ef
marcelldac <dacmarcell@proton.me>
2023-09-19 12:31:22 -0300

    refactor(Footer): mod styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+import { colors } from "../utils/colors";
+
+const container = {
+  backgroundColor: colors.default_bg,
+  bottom: 0,
+  marginTop: 20,
+  paddingTop: 3,
+  position: 'relative',
+  width: '100%',
+  color: 'white',
+  fontSize: '15px',
+  boxShadow: '-1px -5px 5px 0px rgba(0,0,0,0.75)',
+}
+
 export default function Footer() {
   return (
-    <div style={{ width: '100%', height: 'auto' }}>
-      <h1>Footer</h1>
+    <div style={container}>
+      <Link to='/privacidade' style={{ color: 'white' }}>Política de Privacidade</Link>
+      <p>Todos os direitos reservados - © 2023 - DacTI</p>
+      <img src='./logo.png' style={{ marginBottom: -20 }} />
     </div>
   )
 }

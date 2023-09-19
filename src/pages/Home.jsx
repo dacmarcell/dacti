5f5420d6a800cf0af93d07f076b73a9c0eb17a0d
marcelldac <dacmarcell@proton.me>
2023-09-18 22:26:09 -0300

    feat: add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import Block from '../components/Block';
+import BgImg from '../components/BgImg';
+
+export default function Home() {
+
+
+  return (
+    <div>
+      <BgImg />
+      <Block />
+    </div>
+  )
+}

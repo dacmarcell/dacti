f2fa7f3a0960dfe0519824d93770e036f266d63f
marcelldac <dacmarcell@proton.me>
2023-09-18 22:26:44 -0300

    feat: add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+import { colors } from "../utils/colors";
+
+export default function Header() {
+
+  const container = {
+    height: 'auto',
+    width: '100%',
+    backgroundColor: colors.default_bg,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    boxShadow: '10px 10px 30px -20px black',
+  }
+
+  const style = {
+    marginLeft: 200,
+    fontSize: 15,
+  }
+
+  return (
+    <div style={container}>
+      <a href='/'><img src='./logo.png' /></a>
+      <Link style={style} to='/servicos'>Nossos Serviços</Link>
+      <Link style={style} to='/projetos'>Nossos Projetos</Link>
+      <Link style={style} to='/orcamentos'>Orçamento</Link>
+      <Link style={style} to='/contato'>Contato</Link>
+    </div>
+  )
+}

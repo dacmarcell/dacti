850e0466dd119e6b3dd1fbad3f2e116aa6029e00
marcelldac <dacmarcell@gmail.com>
2023-10-08 22:08:19 -0300

    feat: remove "mais" de h2

--- src/pages/Contato.jsx
+++ src/pages/Contato.jsx
@@ -30,7 +30,7 @@ const divIcons = {
 export default function Contato() {
   return (
     <div style={container}>
-      <h2 style={{ color: 'white' }}>Tenha um contato mais direto com o idealizador do projeto!</h2>
+      <h2 style={{ color: 'white' }}>Tenha um contato direto com o idealizador do projeto!</h2>
       <img src='./eu.jpg' style={img} />
       <span style={span}><strong>Marcell Dactes</strong> Desenvolvedor Full-stack apaixonado por tecnologia. Monitor na Unijorge dando diversas palestras de desenvolvimento web a desenvolvimento de APIs. Inspirado em melhorar o mundo com suas ideias.</span>
       <div style={divIcons}>

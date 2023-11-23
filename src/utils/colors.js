5b0c31b496654e2b4105660e4d4e967fd0e85995
marcelldac <dacmarcell@gmail.com>
2023-11-23 04:34:27 -0300

    refactor: removes old pages components and config

--- src/utils/colors.js
+++
@@ -1,6 +0,0 @@
-export const colors = {
-  semi_transparent_red: 'rgba(245, 101, 101, .5)',
-  default_button_bg: 'rgba(229, 62, 62, 1)',
-  default_bg: 'rgba(36, 36, 36, 1)',
-  gray: 'rgba(0, 0, 0, 0.24)',
-}

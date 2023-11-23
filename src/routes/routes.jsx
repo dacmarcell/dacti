33abb2a52158e2ecb54bcccea03f22ac619dc7ac
marcelldac <dacmarcell@gmail.com>
2023-11-23 12:27:14 -0300

    refactor: modify to browserRouter

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Routes, BrowserRouter as Router, BrowserRouter } from 'react-router-dom'
 
 import Home from '../pages/Inicio/Home'
 import Servicos from '../pages/Servicos/Servicos'
@@ -6,12 +6,12 @@ import Projetos from '../pages/Projetos/Projetos'
 
 export default function Navigation() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/servicos" element={<Servicos />} />
         <Route path="/projetos" element={<Projetos />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   )
 }

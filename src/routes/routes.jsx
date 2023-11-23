7a45ef60f25039ff9386d86dc827c5048e678699
marcelldac <dacmarcell@gmail.com>
2023-11-23 12:30:10 -0300

    refactor: hashRouter

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter as Router, BrowserRouter } from 'react-router-dom'
+import { Route, Routes, BrowserRouter as Router, BrowserRouter, HashRouter } from 'react-router-dom'
 
 import Home from '../pages/Inicio/Home'
 import Servicos from '../pages/Servicos/Servicos'
@@ -6,12 +6,12 @@ import Projetos from '../pages/Projetos/Projetos'
 
 export default function Navigation() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/servicos" element={<Servicos />} />
         <Route path="/projetos" element={<Projetos />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
